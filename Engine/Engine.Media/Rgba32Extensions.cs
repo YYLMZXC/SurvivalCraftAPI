e7c7afd38b9a617c092bbbb36ae955eb564acf18
0-v-0 <kduice@qq.com>
2026-03-26 18:15:27 +0800

    优化透明判定
    (cherry picked from commit 8f0fc7759f6142f3bcb637baf0f1b74a1a991fa3)

--- Engine/Engine.Media/Rgba32Extensions.cs
+++ Engine/Engine.Media/Rgba32Extensions.cs
@@ -11,6 +11,6 @@ namespace Engine.Media {
 
         public static bool IsMagenta(this Rgba32 pixel) => pixel.R == 255 && pixel.G == 0 && pixel.B == 255 && pixel.A == 255;
 
-        public static bool IsCompletelyTransparent(this Rgba32 pixel) => pixel.R == 0 && pixel.G == 0 && pixel.B == 0 && pixel.A == 0;
+        public static bool IsCompletelyTransparent(this Rgba32 pixel) => pixel.Rgba == 0;
     }
 }
